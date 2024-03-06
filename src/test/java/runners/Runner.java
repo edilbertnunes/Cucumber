b0c4aa1336c709bcea9f1bcb06f844482278c841
Edilbert Nunes <edilbert.nunes@gmail.com>
2024-03-06 11:05:23 -0300

    Ajuste

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,6 +1,6 @@
 package runners;
 import org.junit.runner.RunWith;
-
+import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
