ad2bef6b96fedcaa9b0abca91f442e118ef52c81
Edilbert Nunes <edilbert.nunes@gmail.com>
2024-03-06 10:59:00 -0300

    Initial commit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,28 @@
+package runners;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/alugarFilme.feature",
+        glue = {"steps"},
+        tags = {"@esse"},
+		// mostra no console o cenário igual o plugin do Cucumber
+		plugin = "pretty", 
+		// retira a configuração para impressão colorida no console (pode criar caracteres especiais)
+		monochrome = false, 
+		
+		// Utiliza CamelCase para criar nome dos métodos
+		snippets = SnippetType.CAMELCASE,
+		
+		// padrão false - valida se o mapeamento está correto, depois volta para false
+		dryRun = false, 
+		
+		// padrão false -caso um passo seja criado e nao implementado será mostrado erro no teste na barra do JUnit
+		strict = false)
+public class Runner {
+
+}
---
+++ src/test/java/steps/AluguarFilmeSteps.java
@@ -0,0 +1,86 @@
+package steps;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+import entidades.Filme;
+import entidades.NotaAluguel;
+import entidades.TipoAluguel;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import servicos.AluguelService;
+import utils.DateUtils;
+
+public class AluguarFilmeSteps {
+	
+	private Filme filme;
+	private AluguelService aluguel = new AluguelService();
+	private NotaAluguel nota;
+	private String erro;
+	private TipoAluguel tipoAluguel = TipoAluguel.COMUM;
+	
+	@Dado("^um filme com estoque de (\\d+) unidades$")
+	public void umFilmeComEstoqueDeUnidades(int arg1) throws Throwable {
+		filme = new Filme();
+	    filme.setEstoque(arg1);
+	}
+
+	@Dado("^que o preço do aluguel seja R\\$ (\\d+)$")
+	public void queOPreçoDoAluguelSejaR$(int arg1) throws Throwable {
+	    filme.setAluguel(arg1);
+	}
+
+	@Quando("^alugar$")
+	public void alugar() throws Throwable {
+		try {
+			nota = aluguel.alugar(filme, tipoAluguel);
+		} catch (RuntimeException e) {
+			erro = e.getMessage();
+		}
+	    
+	}
+
+	@Então("^o preço do aluguel será R\\$ (\\d+)$")
+	public void oPreçoDoAluguelSeráR$(int arg1) throws Throwable {
+	    assertEquals(arg1, nota.getPreco());
+	}
+
+
+	@Então("^o estoque do filme será (\\d+) unidade$")
+	public void oEstoqueDoFilmeSeráUnidade(int arg1) throws Throwable {
+	    assertEquals(arg1, filme.getEstoque());
+	}
+	
+	@Então("^não será possível por falta de estoque$")
+	public void nãoSeráPossívelPorFaltaDeEstoque() throws Throwable {
+	   assertEquals("Filme sem estoque", erro);
+	}
+	
+	@Dado("^que o tipo do aluguel seja (extendido|comum|semanal)$")
+	public void queOTipoDoAluguelSejaExtendido(String tipo) throws Throwable {
+		tipoAluguel = tipo.equals("semanal")? TipoAluguel.SEMANAL: tipo.equals("extendido")? TipoAluguel.EXTENDIDO: TipoAluguel.COMUM;
+	   
+	}
+
+	@Então("^a data de entrega será em (\\d+) dias?$")
+	public void aDataDeEntregaSeráEmDias(int arg1) throws Throwable {
+	   Date dataEsperada = DateUtils.obterDataDiferencaDiaS(arg1);
+	   Date dataReal = nota.getDataEntrega();
+	   
+	   DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+	   
+	   assertEquals(format.format(dataEsperada), format.format(dataReal));
+	}
+
+	@Então("^a pontuação será de (\\d+) pontos$")
+	public void aPontuaçãoSeráDePontos(int arg1) throws Throwable {
+	    assertEquals(arg1, nota.getPontuacao());
+	}
+
+
+}
